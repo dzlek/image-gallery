a73f3f2ae9a33aec76127e6079a6d2bc248dfe12
dzlek <6894255@gmail.com>
2024-09-23 13:37:13 +0300

    feat: add listener change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const KEY = "75WtYzlIo2JmB57Y0Y0N4E-zRTG_4x-VCz4TlI0-v4I";
 const grid = document.getElementById("grid");
+const input = document.getElementById("input");
 
 async function getData(request) {
   try {
@@ -10,17 +11,36 @@ async function getData(request) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
+    grid.innerHTML = "";
+    if (data.results.length === 0) grid.innerHTML = "<div> no results...</div>";
     console.log(data);
     data.results.map((result) => {
-      const img = document.createElement("img");
-      document.body.appendChild(img);
-      img.classList.add("gallery-img");
-      img.src = result.urls.small;
-      img.alt = `image`;
+      createImage(result);
     });
   } catch (error) {
     console.error("Fetch error:", error);
   }
 }
 
-getData("wheel");
+getData("laos");
+
+function createImage(result) {
+  const img = document.createElement("img");
+  const div = document.createElement("div");
+  grid.appendChild(div);
+  div.classList.add("grid_wrapper");
+  div.appendChild(img);
+  img.classList.add("grid_image");
+  img.src = result.urls.regular;
+  img.alt = `image`;
+}
+
+input.addEventListener("change", (event) => {
+  getData(event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (input) {
+    input.focus();
+  }
+});
